f1fa77d0cccdd37e1aeb5d57541d7dda363c2e20
Pohaki <kevin.lachaise@gmail.com>
2016-01-04 10:01:55 +0100

    Corriger

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 $("#browser").text(window.navigator.userAgent);
-$(window).on('resize,load', function(event);
-{ $("width").text(window.innerWidth);
-  $("height").text(window.innerHeight);
+  $(window).on("resize load", function(event){
+    $("#width").text(window.innerWidth);
+    $("#heigth").text(window.innerHeight);
+  });
 });
